d57fb081a8f94b4f61c1a6e52b1774bd845653be
Marcelo Caleb Julio Ofice <marcelocalebjulioo@gmail.com>
2025-01-07 19:21:13 +0200

    TreeSet, CompareTo

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,13 +3,16 @@ package application;
 import entities.Product;
 
 import java.util.HashSet;
+import java.util.Locale;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class Program {
 
     public static void main(String[] args) {
 
-        Set<Product> set = new HashSet<>();
+        Locale.setDefault(Locale.US);
+        Set<Product> set = new TreeSet<>();
 
         set.add(new Product("TV", 900.0));
         set.add(new Product("Notebook", 1200.0));
@@ -18,5 +21,9 @@ public class Program {
         Product product = new Product("Notebook", 1200.0);
 
         System.out.println(set.contains(product));
+
+        System.out.println();
+        for (Product p: set)
+            System.out.println(p);
     }
 }
